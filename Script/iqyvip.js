12d8fe683ec9360979e472664f486abfe029676d
陈鹏 <it_chenpeng@outlook.com>
2020-04-06 19:50:53 +0800

    iqy vip unlock

---
+++ Script/iqyvip.js
@@ -0,0 +1,17 @@
+/*
+iqy unlock vip
+
+QX:
+^http?:\/\/cmonitor\.iqiyi\.com\/apis\/user\/secure_check_vip\.action url script-response-body inetcp/Script/iqyvip.js
+
+*/
+
+var body = $response.body;
+
+// {"msg":"不是会员", "code":"Q00305"}
+let obj = JSON.parse(body);
+obj['msg'] = 'OK';
+obj['code'] = 'Q00000';
+body = JSON.stringify(obj);
+
+$done({body});
